2d276b831676d1697caaaaad10502c9d5372cefd
Tayomide <tibrahim@hawk.iit.edu>
2022-05-01 11:58:04 -0500

    Create Login window

--- src/Login.java
+++ src/Login.java
@@ -1,4 +1,25 @@
+import java.awt.GridLayout;
 
-public class Login {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 
+@SuppressWarnings("serial")
+public class Login extends JFrame{
+	
+	public Login() {
+		
+		setSize(400, 210);
+		setLayout(new GridLayout(4, 2));
+		setLocationRelativeTo(null); // centers window
+		
+		setVisible(true); // SHOW THE FRAME
+	}
+	
+	public static void main(String[] args) {
+		
+		new Login();
+	}
 }
