a3abef2c6844ced71089790005a9fdf616474732
Tayomide <tibrahim@hawk.iit.edu>
2022-05-01 11:58:44 -0500

    Add elements to login page

--- src/Login.java
+++ src/Login.java
@@ -15,6 +15,43 @@ public class Login extends JFrame{
 		setLayout(new GridLayout(4, 2));
 		setLocationRelativeTo(null); // centers window
 		
+		//DEFINE ELEMENTS
+		JLabel lblUsername;
+		JLabel lblPassword;
+		JLabel lblStatus;
+		
+		JTextField txtUname;
+		JPasswordField txtPassword;
+		
+		JButton btn;
+		JButton btnExit;
+		
+		// INITIALIZE ELEMENTS
+		lblUsername = new JLabel("Username", JLabel.LEFT);
+		lblPassword = new JLabel("Password", JLabel.LEFT);
+		lblStatus = new JLabel(" ", JLabel.CENTER);
+		
+		txtUname = new JTextField(10);
+		txtPassword = new JPasswordField();
+		
+		btn = new JButton("Submit");
+		btnExit = new JButton("Exit");
+		
+		// constraints
+
+		lblStatus.setToolTipText("Contact help desk to unlock password");
+		lblUsername.setHorizontalAlignment(JLabel.CENTER);
+		lblPassword.setHorizontalAlignment(JLabel.CENTER);
+ 
+		// ADD OBJECTS TO FRAME
+		add(lblUsername);  // 1st row filler
+		add(txtUname);
+		add(lblPassword);  // 2nd row
+		add(txtPassword);
+		add(btn);          // 3rd row
+		add(btnExit);
+		add(lblStatus);    // 4th row
+		
 		setVisible(true); // SHOW THE FRAME
 	}
 	
