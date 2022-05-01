954d852bdb3b1128e79812d787c166b260c1c841
Tayomide <tibrahim@hawk.iit.edu>
2022-05-01 12:03:23 -0500

    Add functionality to login page

--- src/Login.java
+++ src/Login.java
@@ -1,5 +1,11 @@
-import java.awt.GridLayout;
+import java.awt.GridLayout; //useful for layouts
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+//controls-label text fields, button
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -51,6 +57,36 @@ public class Login extends JFrame{
 		add(btn);          // 3rd row
 		add(btnExit);
 		add(lblStatus);    // 4th row
+		
+		//ADD FUNCTIONALITY
+		btn.addActionListener(new ActionListener() {
+			int count = 0; // count agent
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				boolean admin = false;
+				count = count + 1;
+				// verify credentials of user (MAKE SURE TO CHANGE TO YOUR TABLE NAME BELOW)
+
+				String query = "SELECT * FROM jpapa_users WHERE uname = ? and upass = ?;";
+				try (PreparedStatement stmt = conn.getConnection().prepareStatement(query)) {
+					stmt.setString(1, txtUname.getText());
+					stmt.setString(2, String.valueOf(txtPassword.getPassword()));
+					ResultSet rs = stmt.executeQuery();
+					if (rs.next()) {
+						admin = rs.getBoolean("admin"); // get table column value
+						new Tickets(admin); //open Tickets file / GUI interface
+						setVisible(false); // HIDE THE FRAME
+						dispose(); // CLOSE OUT THE WINDOW
+					} else
+						lblStatus.setText("Try again! " + (3 - count) + " / 3 attempt(s) left");
+				} catch (SQLException ex) {
+					ex.printStackTrace();
+				}
+ 			 
+			}
+		});
+		btnExit.addActionListener(e -> System.exit(0));
 		
 		setVisible(true); // SHOW THE FRAME
 	}
